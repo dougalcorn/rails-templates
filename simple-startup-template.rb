a8b258928b77f0a3c000e28ff945f4462e451790
Doug Alcorn <dougalcorn@gmail.com>
2009-10-26 13:56:30 -0400

    Adding "my" tags:emacs rake task and also don't automatically commit

--- simple-startup-template.rb
+++ simple-startup-template.rb
@@ -8,8 +8,25 @@ doc/app
 run "touch log/.gitignore"
 run "touch tmp/.gitignore"
 
-rake "rails:freeze:gems"
+rake "rails:freeze:gems" unless defined?(RAILS_ROOT) and File.exists?(File.join(RAILS_ROOT, "vendor", "rails"))
+
+rakefile "tags.rake" do
+  <<-TASK
+module Tags
+  RUBY_FILES = FileList['**/*.rb'].exclude("pkg")
+end
+
+namespace "tags" do
+  desc "build the TAGS file using exuberant ctags for emacs"
+  task :emacs do
+    puts "Making Emacs TAGS file"
+    `xctags -e \#\{Tags::RUBY_FILES}`
+  end
+end
+
+task :tags => ["tags:emacs"]
+  TASK
+end
 
 git :init
 git :add => "."
-git :commit => "-a -m 'Initial commit'"
